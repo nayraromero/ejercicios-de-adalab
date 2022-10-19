ab0c15ba12515a98543d3db251155a4f7edc9379
nayraromero <nayraromero@gmail.com>
2022-10-19 10:03:53 +0200

    19/10 exercises

---
+++ modulo-2/modulo-2-leccion-2-apartado-2-ejercicio-1/main.js
@@ -0,0 +1,21 @@
+"use script";
+//Ejercicio 1
+
+const kiwi=5;
+const pera=3;
+const uva=8;
+
+const precioTotal=(kiwi*2)+(pera*3)+(uva*0.5);
+
+console.log(precioTotal);
+
+//Ejercicio 2
+
+const horaDia=24;
+const diaAño=365;
+let edadNayra=25;
+
+const horasVividasNayra=horaDia*diaAño*edadNayra;
+
+console.log(horasVividasNayra);
+
---
+++ modulo-2/modulo-2-leccion-2-apartado-3-ejercicio-1/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const name = "Lola";
+
+console.log("Hola " + name + ", encantada de conocerte.");
+
+
+// Otro ejemplo:
+
+let text = "¿De qué tipo soy?";
+console.log(text + " es del tipo " + typeof text);
+text = 123;
+console.log("ahora " + text + " es del tipo " + typeof text);
+text = undefined;
+console.log("y ahora " + text + " es del tipo " + typeof text);
