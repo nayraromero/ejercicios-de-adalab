fc3a189a86a633ddfe835f15c9580f5f2cff4012
nayraromero <nayraromero@gmail.com>
2022-11-07 19:48:28 +0100

    exercises from 03/11

---
+++ modulo-2/modulo-2-leccion-11-apartado-1-ejercicios/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+//EJERCICIO 1
+// function get100numbers() {
+//     for (let i = 0; i < 100; i ++) {
+//         const array= [] ;
+//         const arrayNumbers=array.push(i);
+//         return arrayNumbers;
+//     }
+    
+// };
+// console.log(get100numbers());
+
+const numbers=[];
+function get100numbers() {
+let loop=0;
+for (i=0; i<100; i++){
+    loop += numbers[i];
+    const numbersArray= numbers.push();
+    console.log(numbersArray);
+};
+};
+
+
+//EJERCICIO 2
+
+// function getReversed100Numbers () {
+//     const array=get100numbers();
+//     array.reverse()
+// };
---
+++ modulo-2/modulo-2-leccion-12-apartado-1-ejercicio/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//EJERCICIO 1
+const marks= [5, 4, 6, 7, 9];
+const inflatedMarks=marks.map((point) => point+1);
+console.log(inflatedMarks);
+
+//EJERCICIO 2
+// const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+// const welcome= names[1].map ((newName) =>
+// newName)
+
+//EJERCICIO 3
